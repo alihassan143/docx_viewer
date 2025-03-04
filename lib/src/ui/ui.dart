6356e9133f41cc56205d8da0e02c3ebb94440f25
oscar <oscar@symterra.co.uk>
2025-03-04 10:58:22 +0000

    added scrollbar, fix Zoom feature, adding padding at the end to improve readability of the last lines of text, use list builder to improve performance of UI

--- lib/src/ui/ui.dart
+++ lib/src/ui/ui.dart
@@ -13,10 +13,20 @@ class DocxViewer extends StatefulWidget {
 
 class _DocxViewerState extends State<DocxViewer> {
   late final Future<List<Widget>> loadDocument;
+  late TransformationController _transformationController;
+  bool _isZooming = false;
+
   @override
   void initState() {
     super.initState();
+    _transformationController = TransformationController();
     loadDocument = DocxExtractor().renderLayout(widget.file);
+  }
+
+  @override
+  void dispose() {
+    _transformationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -26,8 +36,33 @@ class _DocxViewerState extends State<DocxViewer> {
         future: loadDocument,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return InteractiveViewer(
-                child: ListView(children: snapshot.requireData));
+            return GestureDetector(
+              onScaleStart: (_) => setState(() => _isZooming = true), // Detect zoom gesture start
+              onScaleEnd: (_) => setState(() => _isZooming = false),
+              onScaleUpdate: (ScaleUpdateDetails details) {
+                if (details.scale != 1.0) {
+                  setState(() {
+                    _isZooming = true; // Keep zooming enabled
+                  });
+                  _transformationController.value = Matrix4.identity()..scale(details.scale);
+                }
+              },
+              child: InteractiveViewer(
+                child: NotificationListener<ScrollNotification>(
+                  onNotification: (ScrollNotification notification) {
+                    return _isZooming; // Block scroll only when zooming
+                  },
+                  child: Scrollbar(
+                    child: ListView.builder(
+                      padding: const EdgeInsets.only(bottom: 48),
+                      itemCount: snapshot.requireData.length,
+                      physics: _isZooming ? const NeverScrollableScrollPhysics() : const BouncingScrollPhysics(),
+                      itemBuilder: (_, int index) => snapshot.requireData[index],
+                    ),
+                  ),
+                ),
+              ),
+            );
           } else if (snapshot.hasError) {
             return const Center(
               child: Text("Error rendering layout"),
